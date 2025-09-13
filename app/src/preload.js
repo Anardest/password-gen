2f664d4695e59344dc5e8b2b26d0b631e488fb3b
Anardest <associalgamer@gmail.com>
2025-09-13 14:32:21 +0300

    Диалоговое окно

--- app/src/preload.js
+++ app/src/preload.js
@@ -1,5 +1,14 @@
-const { contexBridge, ipcRenderer, contextBridge } = require('electron');
+const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    savePassword: (data) => ipcRenderer.send('save-password', data)
+    savePassword: (data) => ipcRenderer.send('save-password', data),
+    showPrompt: (message) => {
+        // Отправляем запрос на отображение диалога и ждём ответа
+        return new Promise((resolve) => {
+            ipcRenderer.once('prompt-dialog-response', (event, result) => {
+                resolve(result);
+            });
+            ipcRenderer.send('show-prompt-dialog', message);
+        });
+    }
 });
