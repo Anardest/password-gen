b5ce4ca7a3eb3e3e1acaaa89afdcb275ab3b5712
Anardest <associalgamer@gmail.com>
2025-09-26 22:31:48 +0300

    Исправлен путь для build

--- electron-app/src/main.js
+++ electron-app/src/main.js
@@ -3,17 +3,18 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 520,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-  win.loadURL(
-    app.isPackaged
-      ? `file://${path.join(__dirname, '../build/index.html')}`
-      : 'http://localhost:3000'
-  )
+  if (app.isPackaged) {
+    win.loadFile(path.join(process.resourcesPath, 'build', 'index.html'));
+  } else {
+    win.loadURL('http://localhost:3000');
+  }
+
 }
 
 app.whenReady().then(createWindow);
